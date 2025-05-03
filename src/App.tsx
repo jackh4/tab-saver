47c4aab202ba7be26e149dba65e262c5d6ce0a6a
JackH2025 <jackhuang460@gmail.com>
2025-05-03 18:29:21 -0400

    initial implementation queried tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([])
+
+  const onClick = async () => {
+    if (chrome && chrome.tabs) {
+      chrome.tabs.query({}, (result) => {
+        setTabs(result)
+      })
+    } else {
+      console.error('chrome.tabs API is not available')
+    }
+  }
 
   return (
     <>
-      <div>
+    <div>
+        <button onClick={onClick}>Get Links</button>
+      </div>
+      <ul>
+        {tabs.map((tab, index) => (
+          <li key={index}>
+            <a href={tab.url} target="_blank" rel="noopener noreferrer">
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +47,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
