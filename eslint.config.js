c5581e830a02a3faa8949855f7acffbd50486858
jackh4 <jackhuang460@gmail.com>
2025-05-08 23:54:10 -0400

    added ignores for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,10 @@ import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
+  {
+    ignores: ['node_modules', 'dist', 'build', '**/*.config.*'],
+  },
+  { settings: { react: { version: 'detect' } } },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
